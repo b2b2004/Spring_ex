b26dc25066801afc36248116b6c35aa355ed142e
Kwon yongho <b2b2004@nate.com>
2023-04-04 19:37:08 +0900

    3.Object Oriented Principles

--- spring-ex1/src/main/java/com/example/springex1/AppConfig.java
+++ spring-ex1/src/main/java/com/example/springex1/AppConfig.java
@@ -1,23 +1,40 @@
 package com.example.springex1;
 
+import com.example.springex1.discount.DiscountPolicy;
 import com.example.springex1.discount.FixDiscountPolicy;
+import com.example.springex1.discount.RateDiscountPolicy;
+import com.example.springex1.member.MemberRepository;
 import com.example.springex1.member.MemberService;
 import com.example.springex1.member.MemberServiceImpl;
 import com.example.springex1.member.MemoryMemberRepository;
 import com.example.springex1.order.OrderService;
 import com.example.springex1.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 생성자 주입
+    @Bean
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+       //  return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
---
+++ spring-ex1/src/main/java/com/example/springex1/OrderApp.java
@@ -0,0 +1,26 @@
+package com.example.springex1;
+
+import com.example.springex1.member.Grade;
+import com.example.springex1.member.Member;
+import com.example.springex1.member.MemberService;
+import com.example.springex1.order.Order;
+import com.example.springex1.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        // OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+    }
+}
