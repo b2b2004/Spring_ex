1c9878233765552b604b8374f775c274201bb81a
Kwon yongho <b2b2004@nate.com>
2023-04-06 18:41:34 +0900

    5.Singleton

---
+++ spring-ex1/src/test/java/com/example/springex1/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,44 @@
+package com.example.springex1.singleton;
+
+import com.example.springex1.AppConfig;
+import com.example.springex1.member.MemberRepository;
+import com.example.springex1.member.MemberServiceImpl;
+import com.example.springex1.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService ---> memberRepository1 =" + memberRepository1);
+        System.out.println("orderService  ---> memberRepository2 =" + memberRepository2);
+        System.out.println("memberRepository =" + memberRepository);
+
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //출력: bean = class com.example.springex1.AppConfig$$SpringCGLIB$$0
+    }
+}
