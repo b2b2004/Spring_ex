7953b19893af7df571e557f9e4d69a6fe38cfff5
Kwon yongho <b2b2004@nate.com>
2023-06-26 23:35:23 +0900

    11. Spring Advanced - Spring AOP Pointcut

---
+++ spring-aop/src/main/java/com/example/springaop/member/MemberServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.springaop.member;
+
+import com.example.springaop.member.annotation.ClassAop;
+import com.example.springaop.member.annotation.MethodAop;
+import org.springframework.stereotype.Component;
+
+@ClassAop
+@Component
+public class MemberServiceImpl implements MemberService {
+
+    @Override
+    @MethodAop("test value")
+    public String hello(String param) {
+        return "ok";
+    }
+
+    public String internal(String param) {
+        return "ok";
+    }
+}
